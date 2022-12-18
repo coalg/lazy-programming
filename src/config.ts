ce1e1a759878e7de5f6f06e91d51cabf287712fa
eis-shirai <algebroid0@gmail.com>
2022-12-19 03:05:59 +0900

    initial commit

---
+++ src/config.ts
@@ -0,0 +1,57 @@
+export const SITE = {
+	title: 'Nerdy',
+	description: 'Your website description.',
+	defaultLanguage: 'ja_JP',
+};
+
+export const OPEN_GRAPH = {
+	image: {
+		src: 'https://github.com/withastro/astro/blob/main/assets/social/banner-minimal.png?raw=true',
+		alt:
+			'astro logo on a starry expanse of space,' +
+			' with a purple saturn-like planet floating in the right foreground',
+	},
+	twitter: 'astrodotbuild',
+};
+
+// This is the type of the frontmatter you put in the docs markdown files.
+export type Frontmatter = {
+	title: string;
+	description: string;
+	layout: string;
+	image?: { src: string; alt: string };
+	dir?: 'ltr' | 'rtl';
+	ogLocale?: string;
+	lang?: string;
+};
+
+export const KNOWN_LANGUAGES = {
+	Japanese: 'ja',
+} as const;
+export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
+
+export const GITHUB_EDIT_URL = `https://github.com/withastro/astro/tree/main/examples/docs`;
+
+export const COMMUNITY_INVITE_URL = `https://astro.build/chat`;
+
+// See "Algolia" section of the README for more information.
+export const ALGOLIA = {
+	indexName: 'XXXXXXXXXX',
+	appId: 'XXXXXXXXXX',
+	apiKey: 'XXXXXXXXXX',
+};
+
+export type Sidebar = Record<
+	typeof KNOWN_LANGUAGE_CODES[number],
+	Record<string, { text: string; link: string }[]>
+>;
+export const SIDEBAR: Sidebar = {
+	ja: {
+		'Section Header': [
+			{ text: 'Introduction', link: 'ja/introduction' },
+			{ text: 'Page 2', link: 'ja/page-2' },
+			{ text: 'Page 3', link: 'ja/page-3' },
+		],
+		'Another Section': [{ text: 'Page 4', link: 'ja/page-4' }],
+	},
+};
